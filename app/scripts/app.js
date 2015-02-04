76d64588e82da55ad02353797dedb965ba3cee01
Scott Freeman <scott.freeman@midlandhr.co.uk>
2015-02-04 16:10:29 +0000

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,60 @@
+/* global app:true */
+/* exported app */
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ])
+  .constant('FIREBASE_URL', 'https://amber-heat-7876.firebaseio.com/')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
+      .when('/users/:userId', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl',
+        resolve: {
+          user: function(Auth) {
+            return Auth.resolveUser();
+          }
+        }
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl',
+        resolve: {
+          user: function(Auth) {
+            return Auth.resolveUser();
+          }
+        }
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
